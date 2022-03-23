15a6ca02748b748ed3aa2b4a7b98770667ed2ef3
Siniša Krunić <sinisa.krunic@triple-innovations.com>
2022-03-23 12:48:43 +0100

    Add input change handler with delay.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import uuid from "react-uuid";
 import './App.css';
 
 const App = () => {
   const initialList = ["A", "B", "C", "D", "E"];
+  const delay = useRef(null);
 
-  const [itemsList, setItemsList] = useState(initialList); 
+  const [itemsList, setItemsList] = useState(initialList);
+  const [searchValue, setSearchValue] = useState("");
+
+  // Update value on change
+  const onChange = (e) => {
+    clearTimeout(delay.current);
+
+    let value = e.currentTarget.value;
+    setSearchValue(value);
+
+    delay.current = setTimeout(() => {
+      console.log(`API search for: ${value}`);
+    }, 1000);
+  }
 
   return (
     <div className="App">
@@ -15,16 +29,16 @@ const App = () => {
 
       <div className="App-container">
         <div className="App-search">
-          <input type="text" placeholder="Search Band"></input>
+          <input type="text" placeholder="Search Band" onChange={onChange} value={searchValue}></input>
           <ul>
             {
-                itemsList.length > 0 ?
-                    itemsList.map(i => {
-                        return (<li key={uuid()} className="result">{i}</li>)
-                    })
-                    : <li className="result">No result</li>
+              itemsList.length > 0 ?
+                itemsList.map(i => {
+                  return (<li key={uuid()} className="result">{i}</li>)
+                })
+                : <li className="result">No result</li>
             }
-        </ul>
+          </ul>
         </div>
       </div>
     </div>
