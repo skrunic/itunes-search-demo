6319350e519fbc62bf3de187626b9b1a3565d36f
Siniša Krunić <sinisa.krunic@triple-innovations.com>
2022-03-23 19:05:08 +0100

    Add button for initiating search.

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,26 +2,30 @@ import { useState, useRef } from "react";
 
 const SearchField = ({ fetchData }) => {
     const [searchValue, setSearchValue] = useState("");
-    const delay = useRef(null);
+    const [message, setMessage] = useState("");
 
     // Update value on change
     const onChange = (e) => {
-        clearTimeout(delay.current);
-
         let value = e.currentTarget.value;
+        setMessage("");
         setSearchValue(value);
+    }
 
-        delay.current = setTimeout(() => {
-            //console.log(`API search for: ${value}`);
-            if (value.length >= 3) fetchData(value);
-        }, 1000);
+    const searchAction = () => {
+        if (searchValue.length > 0) fetchData(searchValue);
+        else setMessage("Please enter a band name.")
     }
 
     // List render
     return (
         <div className="search-element">
             <input type="text" placeholder="Search Band" onChange={onChange} value={searchValue}></input>
-            <label>Enter at least 3 characters to initiate search.</label>
+            {
+                message?
+                    <label>{message}</label>
+                : null
+            }
+            <button onClick={searchAction}>Search</button>
         </div>
     );
 }
