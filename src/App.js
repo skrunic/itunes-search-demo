c4c44b85e87e4d7c65855009b95eb38e95a1e66c
Siniša Krunić <sinisa.krunic@triple-innovations.com>
2022-03-23 12:37:42 +0100

    Basic input and result list with styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         iTunes Search App
       </header>
+
+      <div className="App-container">
+        <div className="App-search">
+          <input type="text" className="app-search" placeholder="Search Band"></input>
+          <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+            <li>E</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
